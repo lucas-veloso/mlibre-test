9552ebcf85ed7472005b75b5a2e1641718706af0
queara <velosolucas6@gmail.com>
2017-08-25 07:53:13 -0700

    fixing broken tabs

--- rest/test.go
+++ rest/test.go
@@ -11,11 +11,11 @@ import (
 // API response model
 
 type buyer_rep_t struct {
-	Tags				[]string			`json:"tags"`
+	Tags				[]string				`json:"tags"`
 }
 
 type status_t struct {
-	SiteStatus			string				`json:"site_status"`
+	SiteStatus			string					`json:"site_status"`
 }
 
 type ratings_t struct {
@@ -27,37 +27,37 @@ type ratings_t struct {
 type transactions_t struct {
 	Cancelled			int					`json:"cancelled"`
 	Completed			int					`json:"completed"`
-	Period				string				`json:"period"`
-	Ratings				ratings_t			`json:"ratings"`
+	Period				string					`json:"period"`
+	Ratings				ratings_t				`json:"ratings"`
 	Total				int					`json:"total"`
 }
 
 type seller_reputation_t struct {
-	LevelId				string				`json:"level_id"`
-	PowerSellerStatus	string				`json:"power_seller_status"`
-	Transactions   		transactions_t		`json:"transactions"`
+	LevelId				string					`json:"level_id"`
+	PowerSellerStatus		string					`json:"power_seller_status"`
+	Transactions   			transactions_t				`json:"transactions"`
 }
 
 type adress_t struct {
-	City				string				`json:"city"`
-	State				string				`json:"state"`
+	City				string					`json:"city"`
+	State				string					`json:"state"`
 }
 
 type MercadoLibreUser struct { 
-	Id 					int					`json:"id"`
-	Nickname			string 				`json:"nickname"`
-	RegistrationDate	string 				`json:"registration_date"`
-	CountryId			string				`json:"country_id"`
-	Adress				adress_t			`json:"adress"`
-	UserType			string				`json:"user_type"`
-	Logo				string				`json:"logo"`
+	Id 				int					`json:"id"`
+	Nickname			string 					`json:"nickname"`
+	RegistrationDate		string 				`	json:"registration_date"`
+	CountryId			string					`json:"country_id"`
+	Adress				adress_t				`json:"adress"`
+	UserType			string					`json:"user_type"`
+	Logo				string					`json:"logo"`
 	Points				int					`json:"points"`
-	SiteId				string				`json:"site_id"`
-	Permalink			string				`json:"permalink"`
-	Tags				[]string 			`json:"tags"`
-	SellerReputation  	seller_reputation_t	`json:"seller_reputation"`
-	BuyerReputation		buyer_rep_t 	    `json:"buyer_reputation"`
-	Status 				status_t			`json:"status"`
+	SiteId				string					`json:"site_id"`
+	Permalink			string					`json:"permalink"`
+	Tags				[]string 				`json:"tags"`
+	SellerReputation  		seller_reputation_t			`json:"seller_reputation"`
+	BuyerReputation			buyer_rep_t 	    			`json:"buyer_reputation"`
+	Status 				status_t				`json:"status"`
 }
 
 
